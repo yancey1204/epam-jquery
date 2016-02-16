55c3e8fc3c14850388a75ac7edd65758c8b41cde
Kevin Bluer <kevin@bluer.com>
2016-02-16 13:15:17 +0800

    init

---
+++ js/script.js
@@ -0,0 +1,56 @@
+// Note that the following is a shorthand for the DOMReady event
+$(function() {
+
+})
+
+$("#prepend button").click(function() {
+	$("#prepend p").prepend("Yo! ");
+});
+
+$("#before button").click(function() {
+	
+});
+
+$("#css1 button").click(function() {
+	
+});
+
+$("#css2 button").click(function() {
+	
+});
+
+$("#attr1 button").click(function() {
+	
+});
+
+$("#attr2 button").click(function() {
+
+});
+
+$("#class1 button").click(function() {
+	
+});
+
+$("#class2 button").click(function() {
+
+});
+
+$("#form1 button").click(function() {
+
+});
+
+$("#form2 button").click(function() {
+
+});
+
+// handle the mouse over event here
+
+// handle the form events here
+
+$("#form form").submit(function(event) {
+	event.preventDefault();
+
+	debugger;
+
+	// log the values of the email, password, and checkbox (hint for the last one: $("#form input[type=checkbox]").prop("checked"))
+});
